0c3c1e2566b51b7fb1768b9b3dc59a051c709b9d
sho0226 <s1f102201616@iniad.org>
2025-04-02 00:24:14 +0900

    ダイアログの表示

--- src/app/(daily)/20250401/page.tsx
+++ src/app/(daily)/20250401/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import { UserRegistrationDialog } from "./components/UserRegisterationDialog";
 import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <div className={styles.container}>
       <div className={styles.daily}>Daily Note for 20250401</div>
-
-      <div>a</div>
+      <div className={styles.dialog}>
+        <UserRegistrationDialog
+          isOpen={true}
+          userName="Sho"
+          onConfirm={() => console.log("Confirmed")}
+          onCancel={() => console.log("Cancelled")}
+        />
+      </div>
     </div>
   );
 }
