1c29369c0ce7826e90e1044fe8c358d34b6d8e49
sho0226 <s1f102201616@iniad.org>
2025-04-06 00:04:27 +0900

    useeffect

--- src/app/(daily)/20250405/page.tsx
+++ src/app/(daily)/20250405/page.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
 import styles from "./page.module.css";
-      
-export default function Page() { 
-  return <div className={styles.daily}>Daily Note for 20250405</div>; 
+
+export default function Page() {
+  const [text, setText] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+    </div>
+  );
+
+  return <div className={styles.daily}>Daily Note for 20250405</div>;
 }
