82c8e3db0ce89d1ce8e6b3e17f05da05caf35be0
sho0226 <s1f102201616@iniad.org>
2025-04-06 01:15:02 +0900

    useReducer

--- src/app/(daily)/20250405/README.md
+++ src/app/(daily)/20250405/README.md
@@ -1,4 +1,5 @@
 # Daily Note for 20250405
-- [ ] 今日のTODO
-- [ ] 今日の学習内容
-- [ ] 今日の振り返り
+
+- Fetch-on-render
+  - useEffect のコードのようにレンダリング後にフェッチすること
+  -
---
+++ src/app/(daily)/20250406/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useReducer } from "react";
+import styles from "./page.module.css";
+export default function Page() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      default:
+        return state;
+    }
+  }
+
+  return (
+    <div>
+      <div className={styles.daily}>Daily Note for 20250406</div>
+      <p>カウント：{count}</p>
+      <button
+        onClick={() => {
+          dispatch({ type: "increment" });
+        }}
+      >
+        増やす
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "decrement" });
+        }}
+      >
+        減らす
+      </button>
+    </div>
+  );
+}
