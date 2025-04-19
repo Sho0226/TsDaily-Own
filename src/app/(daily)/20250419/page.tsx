dce1049424a70c89004ff5ff9ed54df9934258bf
sho0226 <s1f102201616@iniad.org>
2025-04-20 00:03:57 +0900

    count

--- src/app/(daily)/20250419/page.tsx
+++ src/app/(daily)/20250419/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Page() {
-  return <div className={styles.daily}>Daily Note for 20250419</div>;
+  const [count, setCount] = useState(0);
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+  const handleInclement = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <div className={styles.daily}>Daily Note for 20250419</div>
+      {/* bad */}
+      {/* <button data-testid="decrement-button" onClick={() => handleDecrement()}> */}
+      <button data-testid="decrement-button" onClick={handleDecrement}>
+        -
+      </button>
+      {/* bad */}
+      {/* <button data-testid="increment-button" onClick={() => handleInclement()}> */}
+      <button data-testid="increment-button" onClick={handleInclement}>
+        +
+      </button>
+      <p>clicked: {count}</p>
+    </div>
+  );
 }
